c8dba6377197ef6fc319e11d97253aec3e73949c
udaysinghranaaa <udaysinghranaaa@gmail.com>
2026-02-20 22:34:47 +0530

    this keyword

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var a = 10;
 
 var b = 20;
 
-console.log(name);
-console.log(a+b);
+// console.log(name);
+// console.log(a+b);
 
-console.log(global);
+// console.log(global);
+
+console.log(this);
